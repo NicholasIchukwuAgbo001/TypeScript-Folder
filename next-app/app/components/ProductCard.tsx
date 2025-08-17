eb03e723dbb2c8e0edffbdbae042712652b3e182
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-08-17 03:51:33 +0100

    commit

---
+++ next-app/app/components/ProductCard.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+const ProductCard = () => {
+  return (
+    <div>
+      <button type="submit" onClick={()=> alert("added")}> 
+        Add To Cart 
+      </button>
+    </div>
+  )
+}
+
+export default ProductCard
