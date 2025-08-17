a4a20fea0b2786f831faacb55082ea0f8f80a306
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-08-17 04:20:17 +0100

    commit

--- next-app/app/user/page.tsx
+++ next-app/app/user/page.tsx
@@ -6,12 +6,13 @@ const Page = async () => {
     name: string;
   }
 
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {cache: "no-store"});
   const users: User[] = await res.json();
 
   return (
     <div>
       <h1>User List</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
       <ul>
         {users.map((user) => (
           <li key={user.id}>
