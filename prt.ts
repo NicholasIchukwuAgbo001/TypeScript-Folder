389d89aa717eb2016ba33ea1bc13b36b23563cea
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-08-17 02:04:36 +0100

    commit

--- prt.ts
+++ prt.ts
@@ -12,39 +12,37 @@ console.log("ids:", ids);
 console.log("x:", x);
 console.log("xArray:", xArray);
 
-const concatenateValue = (a: number, b: number) => {
+const concatenateValue = (a: number, b: number): number => {
     return a + b;
 }
 
-console.log(concatenateValue(5, 10))
-
-
+console.log("Concatenated Value:", concatenateValue(5, 10));
 interface UserInterface {
     name: string;
     age?: number;
-    occupation: string
+    occupation: string;
 }
 
 const User: UserInterface = {
     name: "Niko",
     age: 25,
     occupation: "Developer",
-}
+};
 
-if(!User.age){
+if (!User.age) {
     console.log("Age is not defined");
-}else{
-    console.log(User.age)
+} else {
+    console.log("User Age:", User.age);
 }
 
 type IDFieldType = string | number;
 
-const printID = (id: IDFieldType) => {
-    console.log("ID: ",id);
+const printID = (id: IDFieldType): void => {
+    console.log("ID:", id);
 }
 
-printID("1234")
-printID(2)
+printID("1234");
+printID(2);
 
 interface BusinessPartner {
     name: string;
@@ -56,5 +54,26 @@ interface UserIdentity {
     email: string;
 }
 
-type Employer = BusinessPartner & UserIdentity;
+type Employee = BusinessPartner & UserIdentity;
 
+const signContract = (employee: Employee): void => {
+    console.log(
+        "Contract signed with:",
+        employee.name,
+        "at",
+        employee.address,
+        "with ID",
+        employee.id,
+        "and email",
+        employee.email
+    );
+}
+
+const employee: Employee = {
+    name: "Nicholas",
+    address: "123 Main St",
+    id: 101,
+    email: "nicholas@example.com",
+};
+
+signContract(employee);
